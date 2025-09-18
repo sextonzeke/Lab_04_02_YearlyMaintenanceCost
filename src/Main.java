b41800d224aa3414ac91942424e15a84d30e7306
Zeke Sexton <sextonej@mail.uc.edu>
2025-09-18 14:44:22 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double springCost = 1000;
+        double summerCost = 8000;
+        double fallCost = 4000;
+        double winterCost = 3000;
+        double annualCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("The cost for fall " + fallCost + " and the cost for spring " + springCost + " and the cost for winter " + winterCost + " and the cost for summer " + summerCost + " and the cost for annual " + annualCost);
+    }
+}
